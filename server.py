5939ebfd800b77bf592af172e7c94f9879603853
ctamblay <cristian.tamblay@ing.uchile.cl>
2019-04-06 19:08:15 -0300

    Anadido log

--- server.py
+++ server.py
@@ -1,7 +1,12 @@
 import socket
 import sys
+import datetime
+
+def question_isValid():
+    return True
 
 def main(localPort):
+    log_file = open("Log.txt", "w")
     localIP = "127.0.0.1"
 
     bufferSize = 1024
@@ -27,15 +32,23 @@ def main(localPort):
         message = bytesAddressPair[0]
         address = bytesAddressPair[1]
 
-        clientMsg = "Message from Client:{}".format(message)
-        clientIP = "Client IP Address:{}".format(address)
-
+        clientMsg = "mensaje:{}".format(message)
+        clientIP = "IP:{}".format(address)
+        actualTime = datetime.datetime.now()
+        log_file.write("Recibido mensaje del cliente el %s, desde la %s, con el %s." % (actualTime.strftime("%d/%m/%y a las %H:%M:%S"), clientIP, clientMsg))
         print(clientMsg)
         print(clientIP)
 
         # Sending a reply to client
+        # Loggin response
 
         UDPServerSocket.sendto(bytesToSend, address)
+        actualTime = datetime.datetime.now()
+
+        if question_isValid():
+            log_file.write("Respondido a la %s con el mensaje %s, el %s." % (clientIP, 'mensaje del DNS', actualTime.strftime("%d/%m/%y a las %H:%M:%S")))
+        else:
+            log_file.write("La consulta de la %s no es valida el %s" % (clientIP, actualTime.strftime("%d/%m/%y a las %H:%M:%S")))
 
 
 if __name__ == "__main__":
