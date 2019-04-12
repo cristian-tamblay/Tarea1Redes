f7b0f91678ed3637639516bea3eb5fa9349c3dc5
ctamblay <cristian.tamblay@ing.uchile.cl>
2019-04-12 16:17:35 -0400

    Modify resolver and first steps of cache

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ import socket
 import datetime
 import argparse
 import dnsparser
+import pickle
 
 def response_isValid():
     return True
@@ -32,7 +33,8 @@ def main(localPort, dns_resolver):
 
     # Load files for log, cache and filters
     log_file = open("Log.txt", "a")
-    #cache_file = open("Cache.txt", "rw")
+    cache_file = open("Cache.txt", "a")
+    cache_dict = {}
     #filters_file = open("Filters.txt","rw")
 
     localIP = "127.0.0.1"
@@ -63,17 +65,16 @@ def main(localPort, dns_resolver):
                 clientMsg = "mensaje:{}".format(message)
                 clientIP = "IP:{}".format(address)
                 actualTime = datetime.datetime.now()
-                log_file.write("Recibido mensaje del cliente el %s, desde la %s, con el %s." % (actualTime.strftime("%d/%m/%y a las %H:%M:%S"), clientIP, clientMsg))
+                log_file.write("Recibido mensaje del cliente el %s, desde la %s, con el %s.\n" % (actualTime.strftime("%d/%m/%y a las %H:%M:%S"), clientIP, clientMsg))
 
                 # Parse and validate request
-                header, questions = dnsparser.unpackDNS(message)
-                print(header,questions)
-
+                headerC, questionsC = dnsparser.unpackDNS(message)
+                print(headerC,questionsC)
+                id = headerC['ID']
                 # Check if the request is a query
-                if header['QR']:
-                    
+                if headerC['QR']:
                     # Check domain name not in filter
-                    for q in questions:
+                    for q in questionsC:
                         res = requestFiltered(q,None) # TODO
                         if res != "":
                             # Reply to client *cache values should be stored in bytes
@@ -81,8 +82,8 @@ def main(localPort, dns_resolver):
                             actualTime = datetime.datetime.now()
 
                     # Check if domain's in cache
-                    for q in questions:
-                        res = cacheLookup(q,None) # TODO
+                    for q in questionsC:
+                        res = cacheLookup(q, cache_dict) # TODO
                         if res != "":
                             # Reply to client *cache values should be stored in bytes
                             UDPServerSocket.sendto(res, address)
@@ -102,11 +103,15 @@ def main(localPort, dns_resolver):
                     resolverResponse, resolverAddress = UDPProxySocket.recvfrom(BUFFERSIZE)
 
                     # Get the response value's of interest
-                    header, questions = dnsparser.unpackDNS(resolverResponse)
-                    print(header,questions)
-
+                    headerR, questionsR = dnsparser.unpackDNS(resolverResponse)
+                    print(headerR,questionsR)
+                    cache_dict[questionsC]=resolverResponse
+                    #To modify ID:
+                    #resolverModified = bytearray(resolverResponse)
+                    #resolverModified[0],resolverModified[1] = (id+1).to_bytes(2, byteorder='big')[0],(id+1).to_bytes(2, byteorder='big')[1]
+                    #resolverModified = bytes(resolverModified)
                     # Reply to client
-                    UDPServerSocket.sendto(resolverResponse, address)
+                    UDPServerSocket.sendto(resolverModified, address)
                     actualTime = datetime.datetime.now()
 
                     # Loggin response
@@ -126,7 +131,7 @@ def main(localPort, dns_resolver):
     UDPProxySocket.close()
     UDPServerSocket.close()
     log_file.close()
-    #cache_file.close()
+    cache_file.close()
     #filters_file.close()
 
 if __name__ == "__main__":
